e46de735730dc35797470920e2ca29638b4b3c52
“Kent-G93” <kenmwe25@gmail.com>
2023-09-19 21:30:10 +0300

    _printf.c

--- main.h
+++ main.h
@@ -79,12 +79,12 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
