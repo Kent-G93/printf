12d53f9b591eb44b554e091709a8e5cb98cb612c
WinnieKiarago <kiaragowinnie@ymail.com>
2023-09-19 14:14:45 -0700

    get width

--- get_width.c
+++ get_width.c
@@ -12,25 +12,25 @@
 
 int get_width(const char *format, int *i, va_list list)
 {
-        int curnt_i;
-        int width = 0;
+	int curnt_i;
+	int width = 0;
 
-        for (curnt_i = *i + 1; format[curnt_i] != '\0'; curnt_i++)
-        {
-                if (is_digit(format[curnt_i]))
-                {
-                        width *= 10;
-                        width += format[curnt_i] - '0';
-                }
-                else if (format[curnt_i] == '*')
-                {
-                        curnt_i++;
-                        width = va_arg(list, int);
-                        break;
-                }
-                else
-                        break;
-        }
-        *i = curnt_i - 1;
-        return (width);
+	for (curnt_i = *i + 1; format[curnt_i] != '\0'; curnt_i++)
+	{
+		if (is_digit(format[curnt_i]))
+		{
+			width *= 10;
+			width += format[curnt_i] - '0';
+		}
+		else if (format[curnt_i] == '*')
+		{
+			curnt_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+	*i = curnt_i - 1;
+	return (width);
 }
