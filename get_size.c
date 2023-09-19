5307e6bf92af7e70714ff7e7259f0e386e51ba4a
WinnieKiarago <kiaragowinnie@ymail.com>
2023-09-19 14:17:47 -0700

    get size

--- get_size.c
+++ get_size.c
@@ -11,16 +11,16 @@
 
 int get_size(const char *format, int *i)
 {
-        int curnt_i = *i + 1;
-        int size = 0;
+	int curnt_i = *i + 1;
+	int size = 0;
 
-        if (format[curnt_i] == 'l')
-                size = S_LONG;
-        else if (format[curnt_i] == 'h')
-                size = S_SHORT;
-        if (size == 0)
-                *i = curnt_i - 1;
-        else
-                *i = curnt_i;
-        return (size);
+	if (format[curnt_i] == 'l')
+		size = S_LONG;
+	else if (format[curnt_i] == 'h')
+		size = S_SHORT;
+	if (size == 0)
+		*i = curnt_i - 1;
+	else
+		*i = curnt_i;
+	return (size);
 }
